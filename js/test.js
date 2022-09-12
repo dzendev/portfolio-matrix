1ae9dc094a9e7af9cc682ad1de4280d49e17fafa
dzendev <dzendev@gmail.com>
2022-09-12 22:43:10 +0400

    change test.js

--- js/test.js
+++ js/test.js
@@ -1,8 +1,12 @@
 const mA = document.querySelector('#matA');
 const mB = document.querySelector('#matB');
-const btnAddData = document.querySelector('#add-data');
 
-btnAddData.onclick = () => {
+const generateMatrix = document.querySelector('#generate-matrix');
+const div = document.createElement('div');
+div.innerHTML = `<button id="add-data" onclick="addData()">Добавить тестовые данные</button>`;
+generateMatrix.append(div);
+
+function addData() {
 	mA.querySelectorAll('input').forEach(el => el.value = 4);
 	mB.querySelectorAll('input').forEach(el => el.value = 5);
 }
